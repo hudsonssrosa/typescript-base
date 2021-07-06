ad639c14fa0f5d26bfbc21189722c916e3528f37
Mohamed Sheikh <mohamed.sheikh@cazoo.co.uk>
2021-07-06 10:43:08 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/tictactoe.ts
+++ src/main/tictactoe.ts
@@ -1,9 +1,22 @@
 type Player = 'X' | 'O';
 export class TicTacToe {
+
+    private matrix: string[][]
+
+
+    constructor() {
+        this.matrix = [
+            [], [], [],
+            [], [], [],
+            [],[],[]
+        ]
+
+    }
   
-  
-    play(player: Player, position: object) {
-        
-        return 'Played';
+    play(player: Player, position: any) {
+        const posX = position.posX
+        const posY = position.posY
+        this.matrix[posX][posY] = player
+        return this.matrix;
     }
 }
--- src/test/tictactoe.test.ts
+++ src/test/tictactoe.test.ts
@@ -5,6 +5,14 @@ describe("TicTacToe test", () => {
 
   it("Should returns 'Played' when Player 1 has 'X' at position  1,1", () => {
     const position = { posX: 1, posY: 1 }
-    expect(tictactoe.play('X', position)).toBe('Played')
+    const matrix = tictactoe.play('X', position)
+    expect(matrix[1][1]).toBe('X')
   })
+
+  it("Should returns 'position already played' when Player 2 has '0' at position  1,1", () => {
+    const position = { posX: 1, posY: 1 }
+    const matrix = tictactoe.play('O', position)
+    expect(matrix[1][1]).toBe('X')
+  })
+
 });
